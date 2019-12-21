1f8fe5ee8b615291d6165b012c921b8bcfb7e86c
QuangTrung6620 <50941864+QuangTrung6620@users.noreply.github.com>
2019-12-21 22:32:52 +0700

    Commit update

---
+++ Pages/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using T1809E_UWP_DAPI.Utils;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1809E_UWP_DAPI.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+   
+    public sealed partial class Login : Page
+    {
+        private Validator validator = new Validator();
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LoginHanlde(object sender, RoutedEventArgs e)
+        {
+            if (validator.IsEmail(email.Text)){
+                emailAlert.Text = "";
+                if (validator.IsNotNullAndNotEmpty(password.Password)){
+                    passwordAlert.Text = "";
+                    //Next
+                }
+                else
+                {
+                    passwordAlert.Text = "Please enter password";
+                }
+            }
+            else
+            {
+                emailAlert.Text = "Inlavid email, please enter valid email format.";
+            }
+        }
+    }
+}
